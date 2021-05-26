10f0d26076b63a98975417591fdcedc3b5464fac
Denylson-Fuentes <71048462+Denylson-Fuentes@users.noreply.github.com>
2021-05-25 19:48:15 -0700

    Add files via upload

---
+++ src/Pages/App.js
@@ -0,0 +1,74 @@
+import logo from './logo.svg';
+import './App.css';
+import { Switch, Route, useHistory } from 'react-router-dom';
+import HomePage from "./Pages/Home"
+import ContactPage from "./Pages/Contact"
+import PredictPage from "./Pages/Predict"
+import Log from './Pages/Log'
+import {AppBar, Toolbar, IconButton, Typography, Tab, Tabs, Container} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import React, {useState} from 'react';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  appBarStyle:{
+    backgroundColor: "green",
+    padding: "1% 0%"
+  }
+}));
+
+function App() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+  let history = useHistory();
+
+
+  const handleChange = (event, newValue) => {
+    if(newValue === 0){
+      history.push('/')
+    }
+    else{
+      history.push('/predict')
+    }
+    console.log(newValue)
+    setValue(newValue);
+  };
+
+  return (
+    <div className="App">
+      <AppBar position="static" className={classes.appBarStyle}>
+        <Toolbar variant="dense">
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            {/* <Container maxheight="sm">
+              <img src={logo} className="App-logo" alt="logo" width="10%" height="20%"/>
+            </Container> */}
+          </IconButton>
+          <Typography variant="h6" color="inherit">
+            News sentiment Analysis
+          </Typography>
+
+          <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+            <Tab label="Home"  />
+            <Tab label="Predict"  />
+            {/* <Tab label="Item Three"/> */}
+          </Tabs>
+        </Toolbar>
+      </AppBar>
+      <Switch>
+          <Route path="/" exact component={() => <HomePage />} />
+          {/* <Route path="/about" exact component={() => <About />} /> */}
+          <Route path="/contact" exact component={() => <ContactPage />} />
+          <Route path="/predict" exact component={() => <PredictPage />} />
+          <Route path="/log" exact component= {() => <Log/>} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
--- src/Pages/Predict.js
+++ src/Pages/Predict.js
@@ -1,8 +1,10 @@
 
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 import {TextField, Container, Button} from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
-
+import Table from 'react-bootstrap/Table'
+import { Link } from 'react-router-dom';
+import Log from './Log'
 
 const useStyles = makeStyles({
     textContainer: {
@@ -11,8 +13,8 @@ const useStyles = makeStyles({
       borderRadius: 3,
       boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
       color: 'white',
-      height: 48,
-      padding: '0 30px',
+      height: 48,   
+      paddingRight: '80px'
     },
     welcomeContainer:{
         padding: '5% 0'
@@ -24,17 +26,32 @@ const useStyles = makeStyles({
     formContainerStyle:{
         padding: "10% 0",
 
+    },
+    predict:{
+        display : "flex",
+        justifyContent : 'center',
+        alignItems : 'center',
+        
     }
 });
 
 
 function PredictPage(){
     const classes = useStyles();
-    const [text, setName] = React.useState(0);
-    const [result, setResult] = React.useState(0);
+    const [text, setName] = React.useState("");
+    const [result, setResult] = React.useState("");
+    const [show, setShow] = useState(true)
     //const valueRef = useRef('')
     let myRef = React.createRef();
     let input = ""
+
+    const [NegReviews, setNegReviews] = useState(["The movie was very bad", "Production has reset, resulting in product being delayed by 2 weeks"])
+    const [PosReviews, setPosReviews] = useState(["That show was amazing."])
+    const [NeuReviews, setNeuReviews] = useState(["The movie wasn't too bad."])
+
+    const toggleTable = () =>{
+        show  = !show
+    }
 
     const handleSubmit = event =>{
         event.preventDefault();
@@ -52,6 +69,15 @@ function PredictPage(){
         .then((data) => {
         console.log('Success:', data);
         setResult(data)
+        if(data == "Your review is positive"){
+            PosReviews.push(data)
+        }else if (data == "Your review is negative" ){
+            NegReviews.push(data)
+        }
+        else{
+            NeuReviews.push(data)
+        }
+
         })
         //Then with the error genereted...
         .catch((error) => {
@@ -68,7 +94,10 @@ function PredictPage(){
     //   }
     
     return(
-        <div className="PredictPage">
+
+        <div>
+            <div className = {classes.predict} >
+
             <Container maxWidth='sm' className={classes.formContainerStyle}>
                 <form onSubmit={handleSubmit}>
                     <TextField
@@ -88,12 +117,99 @@ function PredictPage(){
                     >
                     </TextField>
                     
+                    
                     <Button variant="contained" color="primary" type="submit">
                         button
                     </Button>
                 </form>
+
+                <h2>{result}</h2>
             </Container>
 
+        </div>
+            
+            
+            <Button onClick = {() => setShow(!show)} >
+            Toggle Table
+            </Button>
+
+            { show == true ? <div style = {{display : "flex",
+                justifyContent : 'center',
+                alignItems : 'center',}}>
+
+                <Container style = {{ width : "70%" }} >
+                    <Table  className="table table-dark table-borderless" variant = 'dark'>
+                        <thead>
+                            <tr  class="text-primary">
+                                <th scope = 'col'>#</th>
+                                <th scope = "col"> Positive Reviews</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {PosReviews.map((item, index)=>{
+                                if( index <= 4){
+                                    return (
+                                        <tr class="table-success" >
+                                            <th scope="row">{index + 1}</th>
+                                            <td>{item}</td>
+                                        </tr>
+                                    )
+                                }
+                            })}
+                        </tbody>
+                    </Table>
+                </Container>
+
+                <Container>
+                    <Table  className="table table-dark table-borderless"  variant = 'dark'>
+                        <thead>
+                            <tr  class="text-primary">
+                                <th scope = 'col'>#</th>
+                                <th scope = "col"> Negative Reviews</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {NeuReviews.map((item, index)=>{
+                                if(index <= 4){
+                                    return (
+                                        <tr class="table-danger">
+                                            <th scope="row">{index + 1}</th>
+                                            <td>{item}</td>
+                                        </tr>
+                                    )
+                                }
+                            })}
+                        </tbody>
+                    </Table>
+                </Container>
+
+                <Container>
+                    <Table  className="table table-dark table-borderless"  variant = 'dark'>
+                        <thead>
+                            <tr  class="text-primary">
+                                <th scope = 'col'>#</th>
+                                <th scope = "col"> Negative Reviews</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {NegReviews.map((item, index)=>{
+                                if(index <= 4){
+                                    return (
+                                        <tr class="table-danger">
+                                            <th scope="row">{index + 1}</th>
+                                            <td>{item}</td>
+                                        </tr>
+                                    )
+                                }
+                            })}
+                        </tbody>
+                    </Table>
+                </Container>
+
+            </div> : 
+                <div> Press Toggle Button </div>
+            }
+        
         </div>
     )
 }
